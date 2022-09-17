96c2865a67058eeedb0d6e8126e2e7c6b815e22f
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-09-17 19:27:20 -0300

    Add notification tool

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import * as Notifications from "expo-notifications";
+
+import { useRef, useEffect } from "react";
 import { StatusBar } from "react-native";
 import {
   useFonts,
@@ -6,10 +9,14 @@ import {
   Inter_700Bold,
   Inter_900Black,
 } from "@expo-google-fonts/inter";
+import { Subscription } from "expo-modules-core";
 
 import { Background } from "./src/components/Background";
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
+
+import "./src/services/notificationConfig";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +25,39 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
 
   if (!fontsLoaded) return <Loading />;
 
